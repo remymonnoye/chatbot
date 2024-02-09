c65645a80f57288f19967ef96fe1ef6a492b7d09
Samanocri <159036714+Samanocri@users.noreply.github.com>
2024-02-09 13:50:46 +0100

    question changement

--- my-social-strength/src/NewOpenAiRequest.js
+++ my-social-strength/src/NewOpenAiRequest.js
@@ -35,8 +35,6 @@ const OpenAIRequest = () => {
         
         const question = "D'apres les discussion par mail que tu as reçu donne moi uniquement les adresse mail de 2 personnes" 
         +"a qui David pourrait envoyer un questionnaire pourrait envoyer un questionnaire pour recueillir des " 
-        const question = "D'apres les discussion par mail que tu as reçu donne moi uniquement les adresse mail des personnes" 
-        +"a qui David pourrait envoyer un questionnaire pour recueillir des " 
         +"feedbacks sur ses qualités et points forts si les discussion sont sans importance "
         +"les gens ne doivent pas etre compté dedans donc ne les indique pas dans le resultat que tu vas me donner " 
         +"analyse bien les discussion pour etre sur que les personne le connaissent bien ou on eu des echange avec lui ?";
